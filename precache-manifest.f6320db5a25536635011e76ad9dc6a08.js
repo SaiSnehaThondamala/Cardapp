a158074a86fb6bcb86505f720d05a13b55cfc471
SaiSnehaThondamala <saisnehathondamala721@gmail.com>
2023-10-31 16:00:45 +0530

    Updates

---
+++ precache-manifest.f6320db5a25536635011e76ad9dc6a08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d0d41288f9ff945bee9",
+    "url": "/Cardapp/static/css/main.2b5093e4.chunk.css"
+  },
+  {
+    "revision": "9d0d41288f9ff945bee9",
+    "url": "/Cardapp/static/js/main.ea27b678.chunk.js"
+  },
+  {
+    "revision": "65fb8cc7d4767c5c4eed",
+    "url": "/Cardapp/static/js/runtime~main.b97d6884.js"
+  },
+  {
+    "revision": "1be2e3cd96aaaee1dc02",
+    "url": "/Cardapp/static/css/2.6c446a0e.chunk.css"
+  },
+  {
+    "revision": "1be2e3cd96aaaee1dc02",
+    "url": "/Cardapp/static/js/2.74880127.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/Cardapp/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "3f9970587f25880cddbf78eeb7422268",
+    "url": "/Cardapp/index.html"
+  }
+];
